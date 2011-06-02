7696d91ba39ecd5e814e978194a0d2258d0a116e
Jim Safley <jimsafley@gmail.com>
2011-06-02 17:35:42 +0000

    Loads first document in viewer on page load.

--- plugin.php
+++ plugin.php
@@ -78,14 +78,24 @@ class DocsViewerPlugin
 <?php if (!empty($docs)): ?>
 <script type="text/javascript">
 jQuery(document).ready(function () {
+    var docviewer = jQuery('#docsviewer_plugin_docviewer');
+    
+    // Set the default docviewer.
+    docviewer.append(
+    '<h2>Viewing: ' + <?php echo js_escape($docs[0]->original_filename); ?> + '</h2>' 
+  + '<iframe src="' + <?php echo js_escape($this->_getUrl($docs[0])); ?> 
+  + '" width="' + <?php echo is_admin_theme() ? js_escape(get_option('docsviewer_width_admin')) : js_escape(get_option('docsviewer_width_public')); ?> 
+  + '" height="' + <?php echo is_admin_theme() ? js_escape(get_option('docsviewer_height_admin')) : js_escape(get_option('docsviewer_height_public')); ?> 
+  + '" style="border: none;"></iframe>');
+    
+    // Handle the document click event.
     jQuery('.docsviewer_plugin_docs').click(function(event) {
-        // Prevent the browser from following the link.
         event.preventDefault();
-        // Get the docviewer and build an iframe.
-        var docviewer = jQuery('#docsviewer_plugin_docviewer');
+        
+        // Reset the docviewer.
         docviewer.empty();
         docviewer.append(
-        '<h2>Document: ' + jQuery(this).text() + '</h2>' 
+        '<h2>Viewing: ' + jQuery(this).text() + '</h2>' 
       + '<iframe src="' + this.href 
       + '" width="' + <?php echo is_admin_theme() ? js_escape(get_option('docsviewer_width_admin')) : js_escape(get_option('docsviewer_width_public')); ?> 
       + '" height="' + <?php echo is_admin_theme() ? js_escape(get_option('docsviewer_height_admin')) : js_escape(get_option('docsviewer_height_public')); ?> 
@@ -94,7 +104,8 @@ jQuery(document).ready(function () {
 });
 </script>
 <div>
-    <h2>View Documents</h2>
+    <h2>Document Viewer</h2>
+    <p>Click below to view a document.</p>
     <ul>
         <?php foreach($docs as $doc): ?>
         <li><a href="<?php echo html_escape($this->_getUrl($doc)); ?>" class="docsviewer_plugin_docs"><?php echo html_escape($doc->original_filename); ?></a></li>
